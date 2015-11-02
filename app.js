f7273e8f3492393d0d247d807ba055ab44f7945f
Klint Tiley <kctiley@gmail.com>
2015-11-02 09:45:25 -0700

    Works for both positive and negative numbers

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ var number = 31337357
 
 var startGame = function(input){
     var result = input;
-    if(result === 1){
+    if(result === 0){
+        console.log(result + ' 1');
         console.log(1)
     }
     else{
-        if(input % 3 === 0){
-            console.log(result + " 0");
-            result = input/3;
-            return startGame(result)
+        if(result === 1){
+            console.log(1)
         }
         else{
-            var subtractOrAdd1 = Math.random();
-            if(subtractOrAdd1 > 0.5){
-                console.log(result + " 1");
-                result += 1;
+            if(input % 3 === 0){
+                console.log(result + " 0");
+                result = input/3;
                 return startGame(result)
             }
             else{
-                console.log(result + " -1");
-                result -= 1;
-                return startGame(result);
+
+                var subtractOrAdd1 = Math.random();
+                if(subtractOrAdd1 > 0.5){
+                    console.log(result + " 1");
+                    result += 1;
+                    return startGame(result)
+                }
+                else{
+                    console.log(result + " -1");
+                    result -= 1;
+                    return startGame(result);
+                }
             }
         }
     }
